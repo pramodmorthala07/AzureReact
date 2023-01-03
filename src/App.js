79e4c435270df3a951afa14d559d9cd845d33508
pramodmorthala07 <pramod.morthala@winfosolutions.com>
2023-01-03 11:28:32 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Azure...
+          Learn React Azure....
         </a>
       </header>
     </div>
